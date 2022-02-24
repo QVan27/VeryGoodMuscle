fc034bdbfc9374dfd4872b42c7220214476e7c62
ImJustLucas <bellierlucas.pro@gmail.com>
2022-02-24 16:05:39 +0100

    ⚡️ add async middleware

--- api.vgm/controllers/users.js
+++ api.vgm/controllers/users.js
@@ -1,95 +1,76 @@
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 const User = require("../models/Users");
 
 //@desc     Get all user
 //@Routes   GET /api/v1/users
 //@access   Public
-exports.getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ success: true, count: users.length, data: users });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({ success: true, count: users.length, data: users });
+});
 
 //@desc     Get one user by Id
 //@Routes   GET /api/v1/users/:id
 //@access   Public
-exports.getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
 
-    if (!user) {
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    next(err);
+  if (!user) {
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
 // @desc    Create new user
 // @route   POST /api/v1/users
 // @access  Private
-exports.createUser = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
 
 // @desc    Update user
 // @route   PUT /api/v1/users/:id
 // @access  Private
-exports.updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!user) {
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
+  if (!user) {
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: user });
+});
 
 // @desc    Delete user
 // @route   DELETE /api/v1/users/:id
 // @access  Private
-exports.deleteUser = (req, res, next) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
 
-    if (!user) {
-      return next(
-        new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      msg: `delete user with id ${req.params.id}`,
-    });
-  } catch (err) {
-    next(err);
+  if (!user) {
+    return next(
+      new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    msg: `delete user with id ${req.params.id}`,
+  });
+});
