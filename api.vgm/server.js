7c31b18eee40403b2ba6ecc82af1bd76a3e6051a
QVan27 <qvannarath@gmail.com>
2022-03-08 16:24:30 +0100

    sparkles: Add Authentication on Api

--- api.vgm/server.js
+++ api.vgm/server.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const cookieParser = require('cookie-parser');
 const errorHandler = require("./middleware/error");
 const connectDb = require("./config/db");
 
@@ -14,13 +16,15 @@ connectDb();
 //Route files
 const users = require("./routes/users");
 const exercices = require("./routes/exercices");
+const auth = require('./routes/auth');
 
 const app = express();
 
-app.use(errorHandler);
-
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // Dev logging middleware
 
@@ -28,9 +32,15 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 //Mout routers
 app.use("/api/v1/users", users);
 app.use("/api/v1/exercices", exercices);
+app.use('/api/v1/auth', auth);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
@@ -38,3 +48,7 @@ app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+});
