c0b25cd34cac62b0876caecfb7c7361575402790
ImJustLucas <bellierlucas.pro@gmail.com>
2022-03-09 17:29:34 +0100

    ✨

--- front.vgm/src/app/pages/profil/profil.page.ts
+++ front.vgm/src/app/pages/profil/profil.page.ts
@@ -1,74 +1,57 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { UsersService } from '../../services/users.service';
+//@ts-ignore
 @Component({
   selector: 'app-profil',
   templateUrl: 'profil.page.html',
-  styleUrls: ['profil.page.scss']
+  styleUrls: ['profil.page.scss'],
 })
 export class ProfilPage {
+  seance = '2,4,6';
+  allUsers: any;
+  apiUrl = 'http://localhost:5000/api/v1/users/6213b3432d12baf306841b20';
 
-  seance = "2,4,6"
+  id = '6213b3432d12baf306841b20';
 
-
-  apiUrl = "http://localhost:5000/api/v1/users/6213b3432d12baf306841b20"
-
-  id = "6213b3432d12baf306841b20"
-
-  constructor(public http:HttpClient) {
+  constructor(public restProvider: UsersService) {
+    // @ts-ignore
+    // this.getAllUsers();
   }
 
-  ngOnInit(){
-    console.log("youppiii")
-    var calendrier = document.getElementById("calendar")
-    var DateNow = new Date
-    var Month = DateNow.getFullYear()+'-0'+(DateNow.getMonth()+1)+'-00';
-    calendrier.setAttribute("min", Month)
-    var Month = DateNow.getFullYear()+'-0'+(DateNow.getMonth()+2)+'-00';
-    calendrier.setAttribute("max", Month)
+  ngOnInit() {
+    console.log('youppiii');
+    var calendrier = document.getElementById('calendar');
+    var DateNow = new Date();
+    var Month = DateNow.getFullYear() + '-0' + (DateNow.getMonth() + 1) + '-00';
+    calendrier.setAttribute('min', Month);
+    var Month = DateNow.getFullYear() + '-0' + (DateNow.getMonth() + 2) + '-00';
+    calendrier.setAttribute('max', Month);
 
     //@ts-ignore
 
-    this.http.get(this.apiUrl).then((result) => {
-      console.log(result, "fhsdjfksfdhlkdsfjsdflkjhdezhkfsdf,nbfbn,kjdsthhfgdktdfgujklgfdgt")
-    }).catch((err) => {
-      console.log(err, "Erreurururururu")
+    this.restProvider.getAllUsers().then((data) => {
+      this.allUsers = data;
+      console.log(this.allUsers);
     });
 
+    // var myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "application/json");
 
-    
-  // var myHeaders = new Headers();
-  // myHeaders.append("Content-Type", "application/json");
-  
-  // var raw = JSON.stringify({
-  //   "last_name": "Maudet"
-  // });
-  
-  // var requestOptions = {
-  //   method: 'PUT',
-  //   headers: myHeaders,
-  //   body: raw,
-  //   redirect: 'follow'
-  // };
-  
-  // fetch("http://localhost:5000/api/v1/users/6213b3432d12baf306841b20", requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(result))
-  //   .catch(error => console.log('error', error));
+    // var raw = JSON.stringify({
+    //   "last_name": "Maudet"
+    // });
+
+    // var requestOptions = {
+    //   method: 'PUT',
+    //   headers: myHeaders,
+    //   body: raw,
+    //   redirect: 'follow'
+    // };
+
+    // fetch("http://localhost:5000/api/v1/users/6213b3432d12baf306841b20", requestOptions)
+    //   .then(response => response.text())
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log('error', error));
   }
-
-
-  getArts() {
-    return new Promise((resolve) => {
-      this.http.get(`${this.apiUrl}`).subscribe(
-        (data) => {
-          resolve(data);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-    });
-  }
-
 }
---
+++ front.vgm/src/app/services/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  apiUrl = 'http://localhost:5000/api/v1/users';
+  constructor(public http: HttpClient) {
+    console.log(`Yes Siiiiiir`);
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+
+  getAllUsers() {
+    return new Promise((resolve) => {
+      this.http.get(`${this.apiUrl}`, this.httpOptions).subscribe(
+        (data) => {
+          resolve(data);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    });
+  }
+}
