740e92deb4e654d3fddfd2d78b2faf6e6274ec4f
QVan27 <qvannarath@gmail.com>
2022-03-10 14:48:33 +0100

    :sparkles: feat: auto log

--- front.vgm/src/app/services/users.service.ts
+++ front.vgm/src/app/services/users.service.ts
@@ -1,5 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +16,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UsersService {
   apiUrl = 'http://localhost:5000/api/v1/users';
   constructor(public http: HttpClient) {
-    console.log(`Yes Siiiiiir`);
   }
 
   httpOptions = {
@@ -19,6 +27,22 @@ export class UsersService {
     }),
   };
 
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  
   getAllUsers() {
     return new Promise((resolve) => {
       this.http.get(`${this.apiUrl}`, this.httpOptions).subscribe(
@@ -32,16 +56,21 @@ export class UsersService {
     });
   }
 
-  getById(id) {
-    return new Promise((resolve) => {
-      this.http.get(`${this.apiUrl}/${id}`, this.httpOptions).subscribe(
-        (data) => {
-          resolve(data);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
-    });
-  }
+  // getById(id: number) {
+  //   return new Promise((resolve) => {
+  //     this.http.get(this.apiUrl + id, this.httpOptions).subscribe(
+  //       (data) => {
+  //         resolve(data);
+  //       },
+  //       (err) => {
+  //         console.log(err);
+  //       }
+  //     );
+  //   });
+  // }
+  // getById(id) {
+  //   return this.http
+  //     .get(this.apiUrl + '/' + id)
+  //     .pipe(retry(2), catchError(this.handleError));
+  // }
 }
